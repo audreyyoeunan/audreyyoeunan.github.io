9f308f1ae381f53b0dc3bc8974e705b0ae25b38b
bsaff <bsaff4@gmail.com>
2025-08-07 19:20:56 -0400

    comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ import { useState } from 'react';
 import Image from 'next/image';
 import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline';
 import HoverCard from '../components/HoverCard';
-
-// Import the JSON data directly
 import siteData from '../../public/data.json';
 
 function isAfter6PMEST() {
